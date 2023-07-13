b2cfd0af52f9c1a79d504a086924ef67d67a28bd
SrikharD <dogiparthysrikhar@gmail.com>
2023-07-13 12:30:59 +0530

    updated NumberManagement.jsx

--- src/Components/NumberManagement.jsx
+++ src/Components/NumberManagement.jsx
@@ -1,37 +1,58 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-function NumberManagement() {
+const NumberManagement = () => {
+  const [urls, setUrls] = useState('');
   const [numbers, setNumbers] = useState([]);
 
-  const getNumbers = () => {
-    const urls = [
-      'http://20.244.56.144/numbers/primes',
-      'http://20.244.56.144/numbers/fibo',
-      'http://20.244.56.144/numbers/odd'
-    ];
+  const handleFetchNumbers = async () => {
+    if (!urls) {
+      alert('Please enter at least one URL.');
+      return;
+    }
 
-    axios.get('/numbers', {
-      params: { url: urls }
-    })
-      .then(response => {
-        setNumbers(response.data.numbers);
-      })
-      .catch(error => {
-        console.log('Error retrieving numbers:', error);
-      });
+    const urlArray = urls.split(',');
+
+    try {
+      const responsePromises = urlArray.map((url) => axios.get(url));
+
+      const responses = await Promise.allSettled(responsePromises);
+      let mergedNumbers = [];
+
+      for (const response of responses) {
+        if (response.status === 'fulfilled') {
+          mergedNumbers = [...mergedNumbers, ...response.value.data.numbers];
+        }
+      }
+
+      const uniqueNumbers = [...new Set(mergedNumbers)].sort((a, b) => a - b);
+      setNumbers(uniqueNumbers);
+    } catch (error) {
+      console.error('Error retrieving numbers:', error);
+      alert('An error occurred while retrieving numbers. Please try again.');
+    }
   };
 
   return (
     <div>
-      <button onClick={getNumbers}>Get Numbers</button>
-      <ul>
-        {numbers.map((number, index) => (
-          <li key={index}>{number}</li>
-        ))}
-      </ul>
+      <h1>Number Management Service</h1>
+      <div>
+        <label htmlFor="urls">Enter URLs (comma-separated):</label>
+        <input type="text" id="urls" value={urls} onChange={(e) => setUrls(e.target.value)} />
+      </div>
+      <button onClick={handleFetchNumbers}>Fetch Numbers</button>
+      {numbers.length > 0 && (
+        <div>
+          <h2>Result:</h2>
+          <ul>
+            {numbers.map((number) => (
+              <li key={number}>{number}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default NumberManagement;
